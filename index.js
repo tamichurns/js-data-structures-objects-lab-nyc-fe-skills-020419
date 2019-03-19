614c9ec39511984834e96757b012d4a7a90c74b5
Tami Churns <tchurns@paperlesspost.com>
2019-03-19 00:37:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  let driverCopy = {...driver}
+  driverCopy[key] = value
+  return driverCopy
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+  let driverCopy = {...driver}
+  delete driverCopy[key]
+  return driverCopy
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+  return driver
+}
